7bf879ca918fe3687c475800df4ad0cadad554cd
Francisco Rios <fcrios145@hotmail.com>
2021-08-05 16:23:24 -0500

    transform-streams-using-map-pluck-and-mapto

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// emit-items-based-on-a-duration-with-interval-and-timer
+import { fromEvent } from 'rxjs';
+import { map, pluck, mapTo } from 'rxjs/operators';
+
+const keyUp$ = fromEvent(document, 'keyup');
+
+const keyCode$ = keyUp$.pipe(
+    map(event => event.code)
+);
+
+const keyCodeWithPlug$ = keyUp$.pipe(
+    pluck('code')
+);
+
+const keyCodeMapTo$ = keyUp$.pipe(
+    mapTo('Key pressed!!')
+);
+
+//keyCode$.subscribe(console.log);
+//keyCodeWithPlug$.subscribe(console.log);
+keyCodeMapTo$.subscribe(console.log);
