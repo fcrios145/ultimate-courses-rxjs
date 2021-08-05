9b75d3c25e9e1a968bdb8e56565a1da50c4f3191
Francisco Rios <fcrios145@hotmail.com>
2021-08-05 16:32:09 -0500

    ignore-unneeded-values-with-filter

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+import { fromEvent } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
+const keyup$ = fromEvent(document, 'keyup');
+
+const keycode$ = keyup$.pipe(
+    map(event => event.code)
+);
+
+const enter$ = keycode$.pipe(
+    filter(
+        code => code === 'Enter'
+    )
+);
+
+enter$.subscribe(console.log);
