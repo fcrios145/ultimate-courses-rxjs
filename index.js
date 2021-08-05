05f503187e4dd19d694324b834b37b09a5680fc6
Francisco Rios <fcrios145@hotmail.com>
2021-08-05 17:15:37 -0500

    accumulate-data-over-time-using-reduce

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+import { from, interval } from 'rxjs';
+import { reduce, take } from 'rxjs/operators';
 
+const numbers = [1,2,3,4,5];
+
+const totalReducer = (acummulator, currentValue) => {
+    console.log(acummulator, currentValue);
+    return acummulator + currentValue
+}
+
+interval(1000).pipe(
+    take(5),
+    reduce(totalReducer, 0)
+).subscribe({
+    next: console.log,
+    complete: () => console.log("Complete!!")
+});
