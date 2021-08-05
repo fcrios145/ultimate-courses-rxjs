14dba3414f72ecd38874c65dc5746298dec52525
Francisco Rios <fcrios145@hotmail.com>
2021-08-05 17:03:33 -0500

    lab-1-create-a-scroll-progress-bar-with-fromevent-and-map

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+function calculateScrollPercent(element) {
+    const {
+        scrollTop,
+        scrollHeight,
+        clientHeight
+    } = element;
+    return (scrollTop / (scrollHeight - clientHeight)) * 100;
+}
+
+const scroll$ = fromEvent(document, 'scroll');
+const progress$ = scroll$.pipe(
+    map(({target}) => calculateScrollPercent(
+        target.scrollingElement
+    ))
+);
+
+const progressBar = document.querySelector('.progress-bar')
+
+progress$.subscribe(percent => {
+    progressBar.style.width = `${percent}%`
+});
