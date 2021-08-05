1f3842b23519de57ca8c5a839f3cba9cbfbe1b2c
Francisco Rios <fcrios145@hotmail.com>
2021-08-05 17:46:38 -0500

    lab-2-create-a-countdown-timer-using-fromevent-map-and-scan

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+import { interval } from 'rxjs';
+import { mapTo, scan, filter } from 'rxjs/operators';
+
+const counter$ = interval(1000);
+const elemCounter = document.getElementById('counter');
+const elemComplete = document.getElementById('complete');
+
+counter$.pipe(
+    mapTo(-1),
+    scan((accumulator, current) => {
+        return accumulator + current;
+    }, 10),
+    filter(value => value >= 0)
+).subscribe(value => {
+    elemCounter.innerHTML = value
+    if(!value) {
+        elemComplete.innerHTML = 'Complete!'
+    }
+});
 
